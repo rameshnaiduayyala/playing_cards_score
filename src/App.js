3de5e46c230b23e0d759ac397f82255273c48204
rameshnaiduayyala <ayyalarameshnaidu@gmail.com>
2023-08-17 18:27:22 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [numPlayers, setNumPlayers] = useState(1);
+  const [playerData, setPlayerData] = useState([]);
+  const [showTable, setShowTable] = useState(false);
+
+  const handleNumPlayersChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    setNumPlayers(newValue);
+  };
+
+  const handleShowTable = () => {
+    setShowTable(true);
+  };
+
+  const handleScoreChange = (index, score) => {
+    const newPlayerData = [...playerData];
+    newPlayerData[index].score = parseInt(score);
+    setPlayerData(newPlayerData);
+  };
+
+  const renderPlayerInputs = () => {
+    const playerInputs = [];
+    for (let i = 0; i < numPlayers; i++) {
+      playerInputs.push(
+        <div key={i}>
+          <input
+            type="text"
+            placeholder={`Player ${i + 1} Name`}
+            onChange={(event) => handlePlayerNameChange(i, event.target.value)}
+          />
+        </div>
+      );
+    }
+    return playerInputs;
+  };
+
+  const handlePlayerNameChange = (index, name) => {
+    const newPlayerData = [...playerData];
+    newPlayerData[index] = { name, score: 0 };
+    setPlayerData(newPlayerData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Score Tracking App</h1>
+      {!showTable && (
+        <>
+          <label>Select number of Players: </label>
+          <input type="number" min="1" value={numPlayers} onChange={handleNumPlayersChange} />
+          <div className="player-inputs">
+            {renderPlayerInputs()}
+          </div>
+          <button onClick={handleShowTable}>Show Table</button>
+        </>
+      )}
+      {showTable && (
+        <div>
+          <h2>Score Table:</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Player Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {playerData.map((player, index) => (
+                <tr key={index}>
+                  <td>{player.name || `Player ${index + 1}`}</td>
+                  <td>
+                    <input
+                      type="number"
+                      placeholder="Enter Score"
+                      value={player.score || 0}
+                      onChange={(event) => handleScoreChange(index, event.target.value)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
